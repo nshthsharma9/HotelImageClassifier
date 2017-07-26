970d7887ea242a1bcf30168deb70fa607d705987
nshthsharma9 <30460698+nshthsharma9@users.noreply.github.com>
2017-07-26 13:34:56 +0530

    Add files via upload

---
+++ Retraining code/src/label_image.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import sys
+import time
+
+start_time = time.time()
+# change this as you see fit
+image_path = sys.argv[1]
+
+
+# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line 
+                   in tf.gfile.GFile("/home/nishith/tensorflow_image_classifier/Trained Model/retrained_labels.txt")]
+
+# Unpersists graph from file
+with tf.gfile.FastGFile("/home/nishith/tensorflow_image_classifier/Trained Model/retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+with tf.Session() as sess:
+    # Feed the image_data as input to the graph and get first prediction
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+	      
+    start_time = time.time()
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+    
+    print (image_path)
+    predictions = sess.run(softmax_tensor, \
+             {'DecodeJpeg/contents:0': image_data})
+    
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    firstElt = top_k[0];
+
+    for node_id in top_k:
+        human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        #print (node_id)
+        print('%s (score = %.5f)' % (human_string, score))
+    print("--- %s seconds ---" % (time.time() - start_time))
